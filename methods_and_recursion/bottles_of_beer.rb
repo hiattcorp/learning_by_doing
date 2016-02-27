f287062fb7ca4fa7cba7e99527e463f4c9f9648b
Jerin Hiatt <jerinhiatt@gmail.com>
2016-02-27 15:22:13 -0700

    Added homework
    [#114625073]

---
+++ methods_and_recursion/bottles_of_beer.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+
+def bottles(number)
+  puts "#{number} of bottles of beer on the wall, #{number} bottles of beer."
+  puts "Take one down, pass it around, #{number -1} bottles of beer on the wall."
+  puts #blank line
+  return if number == 1
+  bottles(number - 1)
+end
+
+puts "#{bottles(99)}"
