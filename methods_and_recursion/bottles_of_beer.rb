40f1a36291b3f49a49011fc129ece1b28dc0f0e9
Jerin Hiatt <jerinhiatt@gmail.com>
2016-02-27 15:31:11 -0700

    Changed for rubocop
    [#114625073]

--- methods_and_recursion/bottles_of_beer.rb
+++ methods_and_recursion/bottles_of_beer.rb
@@ -2,10 +2,10 @@
 
 def bottles(number)
   puts "#{number} of bottles of beer on the wall, #{number} bottles of beer."
-  puts "Take one down, pass it around, #{number -1} bottles of beer on the wall."
-  puts #blank line
+  puts "Take one down, pass it around, #{number - 1} bottles of beer on the wall."
+  puts # blank line
   return if number == 1
   bottles(number - 1)
 end
 
-puts "#{bottles(99)}"
+puts bottles(99).to_s
