77743c8ff2b513f737c11f30cfafb8df5042813b
Jerin Hiatt <jerinhiatt@gmail.com>
2016-02-27 16:09:50 -0700

    Another Travis change
    [#114625073]

--- methods_and_recursion/bottles_of_beer.rb
+++ methods_and_recursion/bottles_of_beer.rb
@@ -2,7 +2,7 @@
 
 def bottles(number)
   puts "#{number} bottles of beer on the wall, #{number} bottles of beer."
-  puts "Take one down, pass it around, #{number - 1} bottles of beer on the wall."
+  puts "Take one down and pass it around, #{number - 1} bottles of beer on the wall."
   puts # blank line
   return if number == 1
   bottles(number - 1)
