a3cab1aa73111924d467a13dc0fe81a4a3a7fca6
Jerin Hiatt <jerinhiatt@gmail.com>
2016-02-28 16:17:27 -0700

    Change
    [#114614031]

--- arrays/array_practice.rb
+++ arrays/array_practice.rb
@@ -21,7 +21,8 @@ puts "The second from last element is #{numbers[8]}"
 # The first four elements are '1, 2, 3, 4'
 puts "The first four elements are '#{numbers[0..3].join', '}'"
 # If we delete 5, 6 and 7 from the array, we're left with [1,2,3,4,8,9,10]
-puts "If we delete #{numbers.slice!(4,3).join(', ')} from the array, we're left with #{numbers.to_s}"
+numbers.slice!(4,3)
+puts "If we delete 5, 6 and 7 from the array, we're left with #{numbers.to_s}"
 # If we add 5 at the beginning of the array, we're left with [5,1,2,3,4,8,9,10]
 puts "If we add 5 at the beginning of the array , we're left with #{numbers.insert(0,5).to_s}"
 # If we add 6 at the end of the array, we're left with [5,1,2,3,4,8,9,10,6]
