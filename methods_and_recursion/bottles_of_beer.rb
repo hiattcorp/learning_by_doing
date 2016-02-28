d9a6f0faee9abe0c8cb99e60939bfdc9b03d8c35
Jerin Hiatt <jerinhiatt@gmail.com>
2016-02-27 23:20:32 -0700

    fix
    [#114625073]

--- methods_and_recursion/bottles_of_beer.rb
+++ methods_and_recursion/bottles_of_beer.rb
@@ -11,16 +11,36 @@
 #puts bottles(99).to_s
 #
 
+#bottles = 99
+#while bottles > 1
+#  puts bottles.to_s + ' bottles of beer on the wall, ' + bottles.to_s + ' bottles of beer.'
+#  puts 'Take one down and pass it around, ' + (bottles-1).to_s + ' bottles of beer on the wall'
+#  bottles = bottles - 1
+#puts # Space for line
+#if bottles == 1
+#    puts bottles.to_s + ' bottle of beer on the wall, ' + bottles.to_s + ' bottle of beer'
+#    puts 'take one down and pass it around, ' + (bottles-1).to_s + ' bottles of beer on the wall'
+#    bottles = bottles - 1
+#
+#  end
+#end
+
 bottles = 99
-while bottles > 1
+while bottles > 2
   puts bottles.to_s + ' bottles of beer on the wall, ' + bottles.to_s + ' bottles of beer.'
   puts 'Take one down and pass it around, ' + (bottles-1).to_s + ' bottles of beer on the wall'
   bottles = bottles - 1
-puts # Space for line
-if bottles == 1
-    puts bottles.to_s + ' bottle of beer on the wall, ' + bottles.to_s + ' bottle of beer'
+  puts # Space for line
+  if bottles == 1
+    puts bottles.to_s + ' bottle of beer on the wall, ' + bottles.to_s + ' bottle of beer.'
     puts 'take one down and pass it around, ' + (bottles-1).to_s + ' bottles of beer on the wall'
     bottles = bottles - 1
 
   end
+
+puts '2 bottles of beer on the wall, 2 bottles of beer.'
+puts 'Take one down and pass it around, 1 bottle of beer on the wall'
+puts # Space for line
+puts '1 bottle of beer on the wall, 1 bottle of beer.'
+puts 'take one down and pass it around, 0 bottles of beer on the wall'
 end
