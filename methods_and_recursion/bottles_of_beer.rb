14d2e9870188b08fee03ec31c02a8dad7130dac9
Jerin Hiatt <jerinhiatt@gmail.com>
2016-02-27 22:52:01 -0700

    Fix
    [#114625073]

--- methods_and_recursion/bottles_of_beer.rb
+++ methods_and_recursion/bottles_of_beer.rb
@@ -1,11 +1,26 @@
 #!/usr/bin/env ruby
 
-def bottles(number)
-  puts "#{number} bottles of beer on the wall, #{number} bottles of beer."
-  puts "Take one down and pass it around, #{number - 1} bottles of beer on the wall."
-  puts # blank line
-  return if number == 1
-  bottles(number - 1)
-end
+#def bottles(number)
+#  puts "#{number} bottles of beer on the wall, #{number} bottles of beer."
+#  puts "Take one down and pass it around, #{number - 1} bottles of beer on the wall."
+#  puts # blank line
+#  return if number == 1
+#  bottles(number - 1)
+#end
+#
+#puts bottles(99).to_s
+#
 
-puts bottles(99).to_s
+bottles = 99
+while bottles > 1
+  puts bottles.to_s + ' bottles of beer on the wall, ' + bottles.to_s + ' bottles of beer.'
+  puts 'Take one down and pass it around, ' + (bottles-1).to_s + ' bottles of beer on the wall'
+  bottles = bottles - 1
+puts # Space for line
+if bottles == 1
+    puts bottles.to_s + ' bottle of beer on the wall, ' + bottles.to_s + ' bottle of beer'
+    puts 'take one down and pass it around, ' + (bottles-1).to_s + ' bottles of beer on the wall'
+    bottles = bottles - 1
+
+  end
+end
