7fc9d034b5277b4a7c9beaabcce3515495328426
Jerin Hiatt <jerinhiatt@gmail.com>
2016-02-29 12:17:09 -0700

    Added cash register homework
    [#11461019]

---
+++ arrays/cash_register.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+
+# register = CashRegister.new
+# register.total  # => 0.00
+# register.purchase(3.78)  # => 3.78
+# register.total  # => 3.78
+# register.pay(5.00)  # => "Your change is $1.22"
+# register.total # => 0.00
+
+
+class CashRegister
+  def purchase
+    puts "Enter cost"
+    @purchase = gets.chomp.to_f
+  end
+  def total
+    @total = @purchase.to_f
+    puts "Total is #{@total.to_f}"
+  end
+  def pay
+    puts "Enter payment"
+    @pay = gets.chomp.to_f
+    puts "Your change is #{@pay-@total}"
+  end
+end
+
+register = CashRegister.new
+register.purchase
+register.total
+register.pay
+
+
