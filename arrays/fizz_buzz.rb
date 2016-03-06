65d66a0ad8bcb0fa81617d7d1eefa92ce6128c69
Jerin Hiatt <jerinhiatt@gmail.com>
2016-03-06 11:04:41 -0700

    Fixed rubocop
    [#114614125]

--- arrays/fizz_buzz.rb
+++ arrays/fizz_buzz.rb
@@ -1,14 +1,14 @@
 #!/usr/bin/env ruby
 
-#This program should output the numbers 1 through 100, however:
+# This program should output the numbers 1 through 100, however:
 #     Output 1-100
-#If evenly divisible by 3, print Fizz
+# If evenly divisible by 3, print Fizz
 #     If number/3 has no remainders print fizz
-#If evenly divisible by 5, print Buzz
+# If evenly divisible by 5, print Buzz
 #     If number/5 has no remainders print buzz
-#If evenly divisible by 3 and 5, print FizzBuzz
+# If evenly divisible by 3 and 5, print FizzBuzz
 #     If number/3 or 5 has no remainder print FizzBuzz
-#Otherwise, print out the number
+# Otherwise, print out the number
 
 # 1.upto(100).each do |number|
 #
@@ -22,16 +22,14 @@
 #   end
 # end
 
-
 numbers = [*1..100]
 numbers.map do |num|
   if num % 3 == 0 && num % 5 == 0
-    puts "FizzBuzz"
+    puts 'FizzBuzz'
   elsif num % 3 == 0
     print'Fizz'
-  elsif num % 5 ==0
+  elsif num % 5 == 0
     print 'Buzz'
   else print num
   end
 end
-
