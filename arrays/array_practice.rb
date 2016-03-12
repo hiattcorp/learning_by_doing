3977c905af6bd2f8e877c4639e3018bbc1b87774
Jerin Hiatt <jerinhiatt@gmail.com>
2016-03-12 08:07:30 -0700

    Fixed homework
    [#114614031]

--- arrays/array_practice.rb
+++ arrays/array_practice.rb
@@ -24,9 +24,11 @@ puts "The first four elements are '#{numbers[0..3].join', '}'"
 numbers.slice!(4,3)
 puts "If we delete 5, 6 and 7 from the array, we're left with [#{numbers.join(',')}]"
 # If we add 5 at the beginning of the array, we're left with [5,1,2,3,4,8,9,10]
-puts "If we add 5 at the beginning of the array , we're left with #{numbers.insert(0,5).to_s}"
+numbers.unshift(5)
+puts "If we add 5 at the beginning of the array , we're left with [#{numbers.join(',')}]"
 # If we add 6 at the end of the array, we're left with [5,1,2,3,4,8,9,10,6]
-puts "If we add 6 at the end of the array, we're left with #{numbers.push(6).to_s}"
+numbers.push(6)
+puts "If we add 6 at the end of the array, we're left with [#{numbers.join(',')}]"
 # Only the elements [9, 10] are > 8.
 puts "Only the elements #{numbers.keep_if{|n| n > 8}} are > 8."
 # If we remove all the elements, then the length of the array is 0
